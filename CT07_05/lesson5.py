ffb644da44cd9d6c054aca6ecb07143781091f2e
potato023 <adrianbon2012@gmail.com>
2025-02-23 07:47:16 +0000

    Feb 23, 2025, 7:47 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,2 +1,22 @@
-print("Hello from lesson 5")
+import random
+pokemons = [
+    "Pikachu", "Charizard", "Bulbasaur", "Squirtle",
+    "Jigglypuff", "Meowth", "Psyduck", "Eevee", "Snorlax",
+    "Mewtwo", "Lapras", "Gengar", "Dragonite", "Machamp",
+    "Arcanine", "Alakazam", "Gyarados", "Vaporeon", "Scyther",
+    "Electabuzz"
+]
+
+powers = [
+    55, 84, 49, 48, 45,
+    45, 52, 55, 110, 110,
+    85, 65, 134, 130, 110,
+    50, 125, 65, 110, 83
+]
+
+
+pokemon_1 = random.choice(pokemons)
+pokemon_2 = random.choice(pokemons)
+power_1 = pokemon_1.index(pokemons)
+power_2 = pokemon_2.index(pokemons)
 
