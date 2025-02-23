81495131c74ccc1a6cd4fcb46f6dab7881c4896f
potato023 <adrianbon2012@gmail.com>
2025-02-23 08:45:44 +0000

    Feb 23, 2025, 8:45 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -12,10 +12,10 @@ contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
 # 2. Write a nested loop to loop through each contact and print
 #    the details for each contact
 
-contacts.append(contact1)
-contacts.append(contact2)
-contacts.append(contact3)
-print (contacts)
+# contacts.append(contact1)
+# contacts.append(contact2)
+# contacts.append(contact3)
+# print (contacts)
 
 
 
