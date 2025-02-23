ee31da06722c7fa5c04dd10a3e1e1aa65d4d5259
potato023 <adrianbon2012@gmail.com>
2025-02-23 08:29:43 +0000

    Feb 23, 2025, 8:29 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -16,3 +16,25 @@ contacts.append(contact1)
 contacts.append(contact2)
 contacts.append(contact3)
 print (contacts)
+
+
+
+# You have been given a 2d list of students from a class
+# consisting each student's name and their gender:
+
+
+students = [
+    ["Olivia", "F"], ["Noah", "M"], ["Emma", "F"],
+    ["Liam", "M"], ["Ava", "F"], ["Ethan", "M"],
+    ["Sophia", "F"], ["Lucas", "M"], ["Mia", "F"],
+    ["Aiden", "M"], ["Isabella", "F"], ["Jackson", "M"],
+    ["Amelia", "F"], ["Logan", "M"], ["Lily", "F"]
+]
+### the above is a nested list. Study and discuss it before we
+### move on.
+
+# 1. Write a for loop to print out the names of each student and
+#    the gender beside.
+   
+#    e.g. Olivia F
+#         Noah M
