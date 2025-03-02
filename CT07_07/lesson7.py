0332dcd08e1632cb3e151d114b9023900ee8add9
potato023 <adrianbon2012@gmail.com>
2025-03-02 08:33:12 +0000

    Mar 2, 2025, 8:33 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,18 +1,23 @@
 
 
-list1 = ["Apple", "Banana", "Cherry", "Durian"]
-list2 = ["Cherry", "Durian", "Elderberry", "Figs"]
-common = []
-unique = []
+# list1 = ["Apple", "Banana", "Cherry", "Durian"]
+# list2 = ["Cherry", "Durian", "Elderberry", "Figs"]
+# common = []
+# unique = []
 
-for fruit in list1:
-    for fruit2 in list2:
-        if fruit == fruit2:
-            common.append(fruit)
+# for fruit in list1:
+#     for fruit2 in list2:
+#         if fruit == fruit2:
+#             common.append(fruit)
 
 
+# for fruit in list1:
+#     for fruit2 in list2:
+#         if fruit == fruit2 == false:
 
 
 print ("common")
 
 
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
