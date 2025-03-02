3dce52a16ba8ae86adeecfee0eb0b91570fbbff9
potato023 <adrianbon2012@gmail.com>
2025-03-02 08:21:07 +0000

    Mar 2, 2025, 8:21 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,22 +1,22 @@
 
-pokemons = [
-    "Pikachu", "Charizard", "Bulbasaur", "Squirtle",
-    "Jigglypuff", "Meowth", "Psyduck", "Eevee", "Snorlax",
-    "Mewtwo", "Lapras", "Gengar", "Dragonite", "Machamp",
-    "Arcanine", "Alakazam", "Gyarados", "Vaporeon", "Scyther",
-    "Electabuzz"
-]
+# pokemons = [
+#     "Pikachu", "Charizard", "Bulbasaur", "Squirtle",
+#     "Jigglypuff", "Meowth", "Psyduck", "Eevee", "Snorlax",
+#     "Mewtwo", "Lapras", "Gengar", "Dragonite", "Machamp",
+#     "Arcanine", "Alakazam", "Gyarados", "Vaporeon", "Scyther",
+#     "Electabuzz"
+# ]
 
-powers = [
-    55, 84, 49, 48, 45,
-    45, 52, 55, 110, 110,
-    85, 65, 134, 130, 110,
-    50, 125, 65, 110, 83
-]
+# powers = [
+#     55, 84, 49, 48, 45,
+#     45, 52, 55, 110, 110,
+#     85, 65, 134, 130, 110,
+#     50, 125, 65, 110, 83
+# ]
 
 
-pokemon_1 = random.choice(pokemons)
-pokemon_2 = random.choice(pokemons)
-power_1 = pokemon_1.index(pokemons)
-power_2 = pokemon_2.index(pokemons)
+# pokemon_1 = random.choice(pokemons)
+# pokemon_2 = random.choice(pokemons)
+# power_1 = pokemon_1.index(pokemons)
+# power_2 = pokemon_2.index(pokemons)
 
