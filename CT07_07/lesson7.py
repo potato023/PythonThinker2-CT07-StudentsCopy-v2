976826cbba17c9524ecff3e2ac0c193417665521
potato023 <adrianbon2012@gmail.com>
2025-03-02 08:14:06 +0000

    Mar 2, 2025, 8:14 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -3,3 +3,6 @@ print("Hello from lesson 7")
 list1 = ["Apple", "Banana", "Cherry", "Durian"]
 list2 = ["Cherry", "Durian", "Elderberry", "Figs"]
 common = []
+
+for fruit in list1:
+    fruit2 in list2
