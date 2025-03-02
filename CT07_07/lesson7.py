5e24a5f87ffd1d558d55181a6c3e8e58d5dc1bd6
potato023 <adrianbon2012@gmail.com>
2025-03-02 08:25:41 +0000

    Mar 2, 2025, 8:25 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -5,9 +5,9 @@ list2 = ["Cherry", "Durian", "Elderberry", "Figs"]
 common = []
 
 for fruit in list1:
-    fruit2 in list2;
-    if fruit == fruit2:
-        common.append(fruit)
+    for fruit2 in list2:
+        if fruit == fruit2:
+            common.append(fruit)
 
 print ("common")
 
