ba8c5a3ca640401afccfd381dadf62e940a8dce1
potato023 <adrianbon2012@gmail.com>
2025-03-09 08:48:58 +0000

    Mar 9, 2025, 8:48 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,26 +1,26 @@
 
 
-import random
-hp = 100
-turns = 0
+# import random
+# hp = 100
+# turns = 0
 
 
 
-import random
-hp = 100
-turns = 0
+# import random
+# hp = 100
+# turns = 0
 
-while hp > 0:
-    (hp) = (hp) - random.randint(1,15)
-    (turns) = (turns) + 1
-    if hp < 1:
-        print ('you have lasted '  ,(turns),  ' turns')
-    else:
-        print ('you now have ' , (hp) , ' left')
-
-
-age = int()
+# while hp > 0:
+#     (hp) = (hp) - random.randint(1,15)
+#     (turns) = (turns) + 1
+#     if hp < 1:
+#         print ('you have lasted '  ,(turns),  ' turns')
+#     else:
+#         print ('you now have ' , (hp) , ' left')
 
 
+age = (input('how old are you'))
+
+print (age)
 
 
