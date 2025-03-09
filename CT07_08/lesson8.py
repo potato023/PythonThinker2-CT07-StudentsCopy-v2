fe3ea1775ae5a262c21ece6179ac8fad03b7052e
potato023 <adrianbon2012@gmail.com>
2025-03-09 08:29:20 +0000

    Mar 9, 2025, 8:29 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -5,12 +5,7 @@ hp = 100
 turns = 0
 
 
-if hp > 0:
-    (hp) = (hp) - random.randint(1,15)
-    (turns) = (turns) + 1
-    print (hp)
-elif hp < 1:
-    print (turns)
+
 
 for i in hp:
     if hp > 0:
