2b037e1fa093bf8ab4715106023ae7edcc2748b9
potato023 <adrianbon2012@gmail.com>
2025-03-09 08:44:18 +0000

    Mar 9, 2025, 8:44 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -12,9 +12,9 @@ while hp > 0:
     (hp) = (hp) - random.randint(1,15)
     (turns) = (turns) + 1
     if hp < 1:
-        print ('you have lasted ' + ('turns') + ' turns')
-    else:
-        print ('you now have ' + (hp) + ' left')
+        print ('you have lasted '  ,('turns'),  ' turns')
+    # else:
+    #     print ('you now have ' + (hp) + ' left')
 
 
 
